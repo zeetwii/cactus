043841b1971b58723e973fc05d2bc3d87436f658
Nick Ashworth <nick.ashworth@dds.mil>
2023-03-05 21:44:38 -0500

    added clustering

--- osprey.py
+++ osprey.py
@@ -8,12 +8,18 @@ from threading import Thread # needed for threads
 
 import pika # needed for rabbitMQ
 
+from sklearn.cluster import KMeans # needed for clustering
+from sklearn.cluster import DBSCAN # needed for DBSCAN clustering
+import numpy as np # needed for clustering
+from sklearn.neighbors import NearestNeighbors # needed for helping find epsilon
+from kneed import KneeLocator # needed for helping to find epsilon
+
 class Osprey:
     """
     Class to handle RF stuff
     """
 
-    def __init__(self, minFreq=1, maxFreq=6000, ampEnable=1, lnaGain=40, vgaGain=30, binSize=100000, dbmAdjust=0):
+    def __init__(self, minFreq=1, maxFreq=6000, ampEnable=1, lnaGain=40, vgaGain=30, binSize=100000, dbmAdjust=0, clusterHistory=60):
         """
         Initalization method
 
@@ -25,6 +31,7 @@ class Osprey:
             vgaGain (int, optional): VGA gain (0-62 dB). Defaults to 40.
             binSize (int, optional): The width of each frequency bin in Hertz. Defaults to 100000.
             dbmAdjust (float, optional): Adds to the calculated power cutoff for minimum dBm to be considered a signal. Defaults to 0.
+            clusterHistory (int, optional): The ammount of previous runs to include when clustering, defaults to 60.
         """
 
         # rabbitMQ setup
@@ -45,6 +52,87 @@ class Osprey:
             self.ampEnable = 1
         else:
             self.ampEnable = 0
+
+        # variables for clustering
+        self.clusterHistory = clusterHistory
+        self.dataList = []
+
+    def __clusterData(self, dataList):
+        """
+        Generates clustering data
+
+        Args:
+            dataList (list): the list of data points to cluster
+
+        Returns:
+            list: list of clustered points
+        """
+
+        # gets distance from all neighbours
+        neighbors = NearestNeighbors(n_neighbors=11).fit(np.asarray(dataList))
+        distances, indices = neighbors.kneighbors(np.asarray(dataList))
+        distances = np.sort(distances[:,10], axis=0)
+
+        # find knee point
+        knee = KneeLocator(np.arange(len(distances)), distances, S=1, curve='convex', direction='increasing', interp_method='polynomial')
+        
+        # use knee point to calculate clusters
+        dbClusters = DBSCAN(eps=distances[knee.knee], min_samples=8).fit(np.asarray(dataList))
+
+        # Number of Clusters
+        nClusters=len(set(dbClusters.labels_))-(1 if -1 in dbClusters.labels_ else 0)
+        #print(str(nClusters))
+        
+        # creates a list of lists to hold each point in its cluster
+        clusterPoints = []
+        for i in range(nClusters):
+            clusterPoints.append([])
+
+        #print(str(clusterPoints))
+
+        # put each pixel in the cluster list its a part of
+        for i in range(len(dataList)):
+            if dbClusters.labels_[i] != -1: # if not a un-clustered point, add to list
+                clusterPoints[dbClusters.labels_[i]].append(dataList[i])
+         
+        # print method for debugging
+        #for i in range(len(clusterPoints)):
+            #print(str(len(clusterPoints[i])))
+
+        return clusterPoints
+
+    def signalCluster(self, newFreq, newDB):
+        """
+        Clusters the detected frequencies into signals.  
+        Uses frequency and signal strength as detection features
+
+        Args:
+            newFreq (list): list of newly detected frequencies
+            newDB (list): list of dBm associated with new frequencies
+        """
+
+        # pair freqs and dB lists
+        tempList = []
+
+        for i in range(len(newFreq)):
+            tempList.append([newFreq[i], newDB[i]])
+
+        self.dataList.append(tempList)
+        #print(str(self.dataList))
+        #print(str(len(self.dataList)))
+
+        if len(self.dataList) > self.clusterHistory:
+            self.dataList.pop(0)
+
+        extendedData = []
+        for data in self.dataList:
+            extendedData.extend(data)
+
+        if len(extendedData) > 1:
+            clusteredData = self.__clusterData(extendedData)
+            print(str(len(clusteredData)))
+
+        
 
     
     def sweepFrequencies(self):
@@ -88,6 +176,9 @@ class Osprey:
 
                     # publish list on rabbitMQ TODO: Change this
                     #self.publishLists(tempFreq, tempDBm, tempPwr, tempDBmPwr)
+
+                    # spawn cluster thread
+                    Thread(target=self.signalCluster, args=(tempPwr, tempDBmPwr), daemon=False).start()
                     
                     # display info for debugging
                     localTime = time.asctime(time.localtime(time.time()))
